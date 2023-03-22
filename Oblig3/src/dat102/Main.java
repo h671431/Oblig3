6bf48617de45739cba15697ba16c9871e2cc16de
hannahhofseth <hannahhofseth@localhost>
2023-03-22 13:35:34 +0100

    t

--- Oblig3/src/dat102/Main.java
+++ Oblig3/src/dat102/Main.java
@@ -4,15 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Integer[] tab = {7, 4, 1, 9, 5, 2};
+		Integer[] tab = {2, 9, 6, 7, 1, 4};
 		
-		SorterTabell.insertionSort(tab, tab.length);
+		InsertionSort.insertionSort(tab, tab.length);
 		
 		for(Integer e : tab) {
 			System.out.println(e + "");
 		}
 		System.out.println();
 	}
-	//test
+	
 
 }
